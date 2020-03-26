876643f85c1d08d966b3df861599a315acae3b45
rodrigocan <rodrigocan@gmail.com>
2020-03-26 17:34:07 -0300

    Parcial 1 do Frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,19 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
 
+import './config/bootstrap'
+import store from './config/store'
+import router from './config/router'
+
 Vue.config.productionTip = false
 
+// TEMPORÁRIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZDNyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODUyNTMzMzcsImV4cCI6MTU4NTUxMjUzN30.l1THVz7uX1M0G6_dVoF1BXeXf6-eeMQ3v88T84of1Xc'
+
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
